6aaecb4a7e432588628ff51c1dad230c4913e81d
Lokesh Goswami <lokesh.goswami.2003@gmail.com>
2025-08-14 18:03:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 let daysOfAYear=(year)=>{
-	return (year%400==0 ||(year%4==0 && year%100!=0))?366:365;
+	return (year%400==0 ||(year%4==0 && year%100!=0))?"366":"365";
 }
-let year=prompt("year");
+let year=prompt("year");  
 alert(daysOfAYear(year));
